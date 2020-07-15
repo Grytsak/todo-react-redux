c9ab6fc1b15cefbf8b59d28589cfe827b60be7ec
Grytsak <grytsak.oleg@gmail.com>
2020-07-15 12:42:09 +0300

    refactor(structure): start to add redux strurcture

--- src/actions/taskAction.js
+++
@@ -1,6 +0,0 @@
-import { DELETE_TASK } from './types';
-
-export function deleteTask() {
-    type: DELETE_TASK,
-    
-}
--- src/actions/types.js
+++
@@ -1,3 +0,0 @@
-export const NEW_TASK = 'NEW_TASK';
-export const DELETE_TASK = 'DELETE_TASK';
-export const COMPLETE_TASK = 'COMPLETE_TASK';
